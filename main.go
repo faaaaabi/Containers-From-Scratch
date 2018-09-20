b00fbabcc0f86fe48c1f0e04f1ab726cf2b6ed10
Fabian <f@bian-meyer.de>
2018-09-20 21:11:01 +0200

    Make the process call it self with another parameter to change hostname

--- main.go
+++ main.go
@@ -11,7 +11,8 @@ func main() {
 	switch os.Args[1] {
 	case "run":
 		run()
-
+	case "child":
+		child()
 	default:
 		panic("bad command")
 	}
@@ -19,6 +20,25 @@ func main() {
 
 func run() {
 	fmt.Printf("Running %v\n", os.Args[2:])
+
+	// ... Unpacking a slices
+	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	// Set OS specific attribute for command execution
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags: syscall.CLONE_NEWUTS, /* Create the Process in a new Namespace */
+	}
+
+	cmd.Run()
+}
+
+func child() {
+	fmt.Printf("Running %v\n", os.Args[2:])
+
+	syscall.Sethostname([]byte("container"))
 
 	// ... Unpacking a slices
 	cmd := exec.Command(os.Args[2], os.Args[3:]...)
