291aaa1190c4fc256d6b46c39e8f9f41ed38313b
Fabian <f@bian-meyer.de>
2018-09-23 20:37:02 +0200

    Add unshare flags

--- main.go
+++ main.go
@@ -29,7 +29,10 @@ func run() {
 
 	// Set OS specific attribute for command execution
 	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags: syscall.CLONE_NEWUTS /* Create the Process in a new Namespace */ | syscall.CLONE_NEWPID,
+		Cloneflags: syscall.CLONE_NEWUTS | /* Create the Process in a new UTS (UNIX Timsharing System) Namespace. Necessary to set domain and hostname */
+			syscall.CLONE_NEWPID | /* Create New PID Namespace */
+			syscall.CLONE_NEWNS, /* Start child in a new mount namespace */
+		Unshareflags: syscall.CLONE_NEWNS, /* Dont share mount namespace with host */
 	}
 
 	cmd.Run()
@@ -41,6 +44,7 @@ func child() {
 	syscall.Sethostname([]byte("container"))
 	syscall.Chroot("/home/fabian/ContainersFromScratch/UbuntuRootFS/ubuntuIMG") // chroot in base image root
 	syscall.Chdir("/")                                                          // Set where you end up after chroot
+	syscall.Mount("proc", "proc", "proc", 0, "")                                // Arguments: src, target, festype, flags, data. Mount proc to the "container"
 
 	// ... Unpacking a slice
 	cmd := exec.Command(os.Args[2], os.Args[3:]...)
@@ -54,4 +58,6 @@ func child() {
 	}
 
 	cmd.Run()
+
+	syscall.Unmount("/proc", 0) // Unmount after we are finished
 }
